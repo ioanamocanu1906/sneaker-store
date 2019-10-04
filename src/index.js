d0f2d45aa03e8ba444657ec45434f255e746789e
Mocanu <ioana.mocanu@metrosystems.net>
2019-10-04 12:49:31 +0300

    test

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const router = (
   </Provider>
 );
 render(router, document.getElementById("root"));
-
+console.log();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
